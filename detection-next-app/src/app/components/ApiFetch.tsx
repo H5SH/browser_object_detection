6e33ce5d8bb9c206842a1ee6305a2e5859ae8723
Hasham Asad <tipuasad2001@gmail.com>
2024-09-16 04:36:02 +0500

    connectionDone

---
+++ detection-next-app/src/app/components/ApiFetch.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 10000, 
+});
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (!error.response) {
+      console.log(error)
+    }
+    return Promise.reject(error);
+  }
+);
+
+export const ApiFetchReq = (methods: string, url: string, body: object) => {
+  const token = localStorage.getItem('token');
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const config = {
+    method: methods,
+    url,
+    data: body,
+    headers,
+  };
+  return instance(config)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      // Handle other errors here, if needed
+      console.error(err);
+      throw err; // Re-throw the error to propagate it to the calling function
+    });
+};
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello")
+from typing import Union
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/upload/file")
+def read_root(file: UploadFile):
+    return {"Hello": file.filename}
