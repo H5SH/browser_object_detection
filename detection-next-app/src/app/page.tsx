de810f747ee783a32c3b43ca8f47a7a7a36e970f
Hasham Asad <tipuasad2001@gmail.com>
2024-09-18 22:19:57 +0500

    streamingAndDetection

--- detection-next-app/src/app/page.tsx
+++ detection-next-app/src/app/page.tsx
@@ -6,19 +6,43 @@ import { ApiFetchReq } from "./components/ApiFetch";
 export default function Home() {
 
   const [file, setFile] = useState<Blob | null>()
-  
+  const [responses, setResponses] = useState<Array<any>>([])
+
   async function submitFile(e: any) {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", file as Blob);    
-    const response = await ApiFetchReq('POST', 'http://127.0.0.1:8000/upload/file', formData)
-    console.log("results", response)
+    try{
+      const formData = new FormData();
+      formData.append("file", file as Blob);
+      const response = await fetch('http://127.0.0.1:8000/upload/file', {
+        method: 'POST',
+        body: formData
+      })
+      const reader: any = response.body?.getReader();
+      const decoder = new TextDecoder('utf-8');
+      let chunk = '';
+  
+      while(true){
+        const {done, value} = await reader?.read();
+        if(done) break;
+  
+        chunk = decoder.decode(value, {stream: true});
+  
+        const parsedData = JSON.parse(chunk)
+
+        setResponses([...responses, parsedData])
+  
+      }
+    }catch(er){
+      console.log(er)
+    }
+    // setResponse(response)
   }
 
   return (
     <div>
       <input type="file" onChange={(e) => setFile(e.target.files!![0])} />
       <input type="button" onClick={submitFile} value={'submit'} />
+      {/* {response && <div>{response}</div>} */}
     </div>
   );
 }
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, UploadFile, File
-from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from ultralytics import YOLO
 from PIL import Image
 import io
+import json
 
 app = FastAPI()
 
@@ -31,7 +31,7 @@ async def identifying_object_stream(image):
     capacitor_detected = capacitor_detector(image)
     
     if len(capacitor_detected[0].boxes) > 0:
-        yield b"Capacitor Detected"
+        yield json.dumps({"status": "detected", "type": "capacitor"}).encode('utf-8')
         for i, result in enumerate(capacitor_detected[0].boxes):
             box = result.xyxy[0].cpu().numpy()
             x1, y1, x2, y2 = map(int, box)
@@ -41,23 +41,19 @@ async def identifying_object_stream(image):
             terminal_detected = terminal_detector(cropped_img)
 
             if len(terminal_detected[0].boxes) > 0:
-                yield b"Terminal Detected"
+                class_ids = terminal_detected[0].boxes.cls
+
+                detected_objects = [terminal_detector.names[int(cls_id)] for cls_id in class_ids]
+                yield json.dumps({"status": "detected", "type": "terminal", "detected_objects": detected_objects}).encode('utf-8')
+            else:
+                yield json.dumps({"status": "notDetected", "type": "terminal"}).encode('utf-8')
 
     else:
-        yield "Failed To detect Capacitor"
-
-    # print(capacitor_detected, 'detected')
-
-    # annotated_image = capacitor_detected[0].plot()
-
-    # img_byte_arr = io.BytesIO()
-    # Image.fromarray(annotated_image).save(img_byte_arr, format='PNG')
-    # img_byte = img_byte_arr.getvalue()
+        yield json.dumps({"status": "notDetected", "type": "capacitor"}).encode('utf-8')
 
 @app.post("/upload/file")
 async def read_root(file: UploadFile = File()):
     content = await file.read()
     image = Image.open(io.BytesIO(content))
 
-    # print(img_byte, 'img')
-    return StreamingResponse(identifying_object_stream(image))
+    return StreamingResponse(identifying_object_stream(image),  media_type="application/octet-stream")
