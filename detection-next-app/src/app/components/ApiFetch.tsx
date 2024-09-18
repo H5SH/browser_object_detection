4dc9a3cc1071d39a81c058f00f5718a2cb134376
Hasham Asad <tipuasad2001@gmail.com>
2024-09-18 05:26:36 +0500

    predictedFile

--- detection-next-app/src/app/components/ApiFetch.tsx
+++ detection-next-app/src/app/components/ApiFetch.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 10000, 
+  timeout: 1000000, 
 });
 
 instance.interceptors.response.use(
