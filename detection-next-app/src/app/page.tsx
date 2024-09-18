66b05406baa17e8c67fe6b3aeab18a3263eeb3c8
Hasham Asad <tipuasad2001@gmail.com>
2024-09-19 01:56:40 +0500

    'detectionAndFailureDisplayed'

--- detection-next-app/src/app/page.tsx
+++ detection-next-app/src/app/page.tsx
@@ -1,16 +1,22 @@
 "use client"
 
 import { useState } from "react";
-import { ApiFetchReq } from "./components/ApiFetch";
+
+interface ResponseObj {
+  detected: Boolean,
+  type: String,
+  detected_objects?: Array<String>
+}
 
 export default function Home() {
 
   const [file, setFile] = useState<Blob | null>()
-  const [responses, setResponses] = useState<Array<any>>([])
+  const [responses, setResponses] = useState<Array<ResponseObj>>([])
 
   async function submitFile(e: any) {
     e.preventDefault();
     try{
+      setResponses([])
       const formData = new FormData();
       formData.append("file", file as Blob);
       const response = await fetch('http://127.0.0.1:8000/upload/file', {
@@ -27,22 +33,33 @@ export default function Home() {
   
         chunk = decoder.decode(value, {stream: true});
   
-        const parsedData = JSON.parse(chunk)
+        const parsedData: ResponseObj = JSON.parse(chunk);
 
-        setResponses([...responses, parsedData])
-  
+        setResponses((prevState)=> [...prevState, parsedData])
       }
     }catch(er){
       console.log(er)
     }
-    // setResponse(response)
   }
+
 
   return (
     <div>
       <input type="file" onChange={(e) => setFile(e.target.files!![0])} />
       <input type="button" onClick={submitFile} value={'submit'} />
-      {/* {response && <div>{response}</div>} */}
+      {responses.map((response: ResponseObj, index)=>(
+        <div key={index}>
+          {response.detected ? (
+            <div className="text-green-400">
+              {`Detected ${response.type}`}
+              </div>
+          ):(
+            <div className="text-red-600">
+              {`Failed To Detect ${response.type}`}
+            </div>
+          )}
+          </div>
+      ))}
     </div>
   );
 }
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def identifying_object_stream(image):
     capacitor_detected = capacitor_detector(image)
     
     if len(capacitor_detected[0].boxes) > 0:
-        yield json.dumps({"status": "detected", "type": "capacitor"}).encode('utf-8')
+        yield json.dumps({"detected": True, "type": "capacitor"}).encode('utf-8')
         for i, result in enumerate(capacitor_detected[0].boxes):
             box = result.xyxy[0].cpu().numpy()
             x1, y1, x2, y2 = map(int, box)
@@ -44,12 +44,12 @@ async def identifying_object_stream(image):
                 class_ids = terminal_detected[0].boxes.cls
 
                 detected_objects = [terminal_detector.names[int(cls_id)] for cls_id in class_ids]
-                yield json.dumps({"status": "detected", "type": "terminal", "detected_objects": detected_objects}).encode('utf-8')
+                yield json.dumps({"detected": True, "type": "terminal", "detected_objects": detected_objects}).encode('utf-8')
             else:
-                yield json.dumps({"status": "notDetected", "type": "terminal"}).encode('utf-8')
+                yield json.dumps({"detected": False, "type": "terminal"}).encode('utf-8')
 
     else:
-        yield json.dumps({"status": "notDetected", "type": "capacitor"}).encode('utf-8')
+        yield json.dumps({"detected": False, "type": "capacitor"}).encode('utf-8')
 
 @app.post("/upload/file")
 async def read_root(file: UploadFile = File()):
